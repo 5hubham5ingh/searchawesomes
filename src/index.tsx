85a5523766d0524f2b0ead2ecb6aa83a6b828d2d
ss <ss.dev.me@gmail.con>
2025-03-22 01:57:02 +0530

    Add: theme switch button component

--- src/index.tsx
+++ src/index.tsx
@@ -14,8 +14,23 @@ export function App() {
 }
 
 function Header() {
+  const switchTheme = () => {
+    const theme = document.documentElement.getAttribute("data-theme");
+    if (theme === "light") {
+      document.documentElement.style.setProperty("--background", "black");
+      document.documentElement.style.setProperty("--foreground", "gray");
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.style.setProperty("--foreground", "black");
+      document.documentElement.style.setProperty("--background", "gray");
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  };
   return (
     <header>
+      <button onClick={switchTheme} type="button">
+        Switch Theme
+      </button>
       <h1>Search Awesomes</h1>
       <p>
         Search from hundreds of awesome lists of tools and resources maintained
@@ -37,7 +52,7 @@ function Search() {
 function SearchResults() {
   return (
     <div id="searchResults">
-      {(new Array(300).fill(0)).map((num, i) => (
+      {new Array(300).fill(0).map((num, i) => (
         <fieldset className="card">
           <legend>Search Result {i + 1}</legend>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
@@ -53,11 +68,7 @@ function SearchResults() {
 function ProjectLink() {
   return (
     <a href="https://github.com/5hubham5ingh/searchawesomes">
-      <svg
-        viewBox="0 0 20 20"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+      <svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <g
           id="Page-1"
           stroke="none"
@@ -74,8 +85,7 @@ function ProjectLink() {
               <path
                 d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"
                 id="github-[#142]"
-              >
-              </path>
+              ></path>
             </g>
           </g>
         </g>
