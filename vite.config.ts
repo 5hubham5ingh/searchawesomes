d544b39f2045ca0c4e2a806e6a0d996827455aab
ss <ss.dev.me@gmail.con>
2025-03-21 20:40:43 +0530

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  server: {
+    hmr: {
+      host: "192.168.200.1",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   base: "/searchawesome/",
 });
